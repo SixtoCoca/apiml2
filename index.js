60586236a4bba8fde081969a481fc92ed1c2694c
Sixto <sixtococacruz@gmail.com>
2021-02-16 20:28:19 +0100

    Avanzando con express

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import bodyparser from "body-parser";
 const server = express();
-const mostrarPantalla = (peticion, respuesta) => {
+server.use(bodyparser.json());
+
+server.get("/", (peticion, respuesta) => {
   respuesta.json({
     mensaje: "hola",
     peticion: peticion.headers,
   });
-};
-server.get("/", mostrarPantalla);
+});
 
-server.listen(3000);
+server.post("/", (peticion, respuesta) => {
+  respuesta.json({
+    edad: peticion.body.edad,
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log("Escuchando en http://localhost:" + port);
+});
