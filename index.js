1828fba4e6ab349c8897572812936902265ea6ec
Sixto <sixtococacruz@gmail.com>
2021-02-16 23:03:14 +0100

    Finalizado Machine Learning

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyparser from "body-parser";
 import vision from "@google-cloud/vision";
+import multer from "multer";
 
 const server = express();
 const client = new vision.ImageAnnotatorClient();
+const upload = multer({ dest: "uploads/" });
 
 server.use(bodyparser.json());
 
@@ -20,13 +22,19 @@ server.post("/", (peticion, respuesta) => {
   });
 });
 
-server.post("/inferencia", async (peticion, respuesta) => {
-  const [result] = await client.labelDetection("./mono.jpg");
-  const labels = result.labelAnnotations;
-  respuesta.json({
-    resultado: labels,
-  });
-});
+server.post(
+  "/inferencia",
+  upload.single("imagen"),
+  async (peticion, respuesta) => {
+    const [result] = await client.labelDetection(
+      peticion.file.destination + peticion.file.filename
+    );
+    const labels = result.labelAnnotations;
+    respuesta.json({
+      resultado: labels,
+    });
+  }
+);
 
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
